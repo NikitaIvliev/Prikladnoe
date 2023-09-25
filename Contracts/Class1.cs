82912ca9f0bc6029e036985d21064929f8aa4fb5
Никита Ивлиев <list2442@mail.ru>
2023-09-25 20:04:10 +0300

    2

--- Contracts/Class1.cs
+++ Contracts/Class1.cs
@@ -1,7 +1,17 @@
-﻿namespace Contracts
+﻿using static Contracts.Contracts;
+
+namespace Contracts
 {
     public class Contracts
     {
+        public interface ICompanyRepository
+        {
+            void AnyMethodFromCompanyRepository();
+        }
+        public interface IEmployeeRepository
+        {
+            void AnyMethodFromEmployeeRepository();
+        }
 
     }
     public interface ILoggerManager
@@ -10,5 +20,13 @@
         void LogWarn(string message);
         void LogDebug(string message);
         void LogError(string message);
+
     }
+    public interface IRepositoryManager
+    {
+        ICompanyRepository Company { get; }
+        IEmployeeRepository Employee { get; }
+        void Save();
+    }
+
 }
